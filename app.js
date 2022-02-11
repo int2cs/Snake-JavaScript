b4d682819354d4c5c2022bb39c506dbe553775cd
int2cs <gabriel.gn87@protonmail.com>
2022-02-11 19:19:25 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,134 @@
+/*
+  DECLARATIONS
+*/
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+let direction = "right";
+let apple = [5, 5];
+let score = 0;
+const speed = 800;
+const gridElem = 40; //40 => taille de chaque case de notre grille soit 20 cases * 20 case = 800(largeur et hauteur de notre grille)
+const snake = [
+  [9, 9],
+  [8, 9],
+  [7, 9],
+]; //initial position [[x, y], [x, y], [x, y]]
+
+window.addEventListener("keydown", keyboardEvent);
+
+requestAnimationFrame(play);
+
+/*
+  FUNCTIONS
+*/
+function drawMap() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 800, 800);
+}
+function drawScore() {
+  const text = `Score : ${score}`;
+  ctx.fillStyle = "white";
+  ctx.font = "30px Verdana";
+  ctx.textAlign = "right";
+  ctx.textBaseline = "top";
+  const textWidth = ctx.measureText(text);
+
+  ctx.fillText(text, canvas.width - textWidth.width, 30);
+}
+function drawSnake() {
+  ctx.fillStyle = "yellow";
+  for (let body of snake) {
+    ctx.fillRect(body[0] * gridElem, body[1] * gridElem, gridElem - 2, gridElem - 2);
+  }
+}
+function drawApple() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(apple[0] * gridElem, apple[1] * gridElem, gridElem, gridElem);
+}
+function generateApple() {
+  const [x, y] = [Math.trunc(Math.random() * 19), Math.trunc(Math.random() * 19)];
+
+  for (let bodyElem of snake) {
+    if (bodyElem[0] === x && bodyElem[1] === y) return generateApple();
+  }
+  apple = [x, y];
+}
+
+function isGameOver() {
+  if (snake[0][0] > 19 || snake[0][0] < 0 || snake[0][1] > 19 || snake[0][1] < 0) return true;
+  else {
+    const [head, ...body] = snake; // On destructure snake pour récupérer uniquement la tête et le reste de la queue
+    for (let bodyElem of body) {
+      if (head[0] === bodyElem[0] && head[1] === bodyElem[1]) return true;
+    }
+  }
+
+  return false;
+}
+function isEating() {
+  if (snake[0][0] === apple[0] && snake[0][1] === apple[1]) {
+    generateApple();
+    score++;
+    return true;
+  }
+}
+
+function play() {
+  updateSnakePosition();
+
+  if (!isGameOver()) {
+    drawMap();
+    drawSnake();
+    drawApple();
+    drawScore();
+
+    setTimeout(() => {
+      requestAnimationFrame(play);
+    }, 1000 - speed);
+  } else {
+    console.log("GAME OVER !");
+  }
+}
+
+function updateSnakePosition() {
+  let head;
+  switch (direction) {
+    case "right":
+      head = [snake[0][0] + 1, snake[0][1]];
+      break;
+    case "bottom":
+      head = [snake[0][0], snake[0][1] + 1];
+      break;
+    case "left":
+      head = [snake[0][0] - 1, snake[0][1]];
+      break;
+    case "top":
+      head = [snake[0][0], snake[0][1] - 1];
+      break;
+
+    default:
+      break;
+  }
+  snake.unshift(head); // On ajoute la tête à la place du première élément du notre tableau snake
+  if (!isEating()) snake.pop(); // On supprime le dernier élément du notre tableau snake si il ne mange pas de pomme
+}
+
+function keyboardEvent(e) {
+  switch (e.code) {
+    case "ArrowUp":
+      direction = "top";
+      break;
+    case "ArrowDown":
+      direction = "bottom";
+      break;
+    case "ArrowLeft":
+      direction = "left";
+      break;
+    case "ArrowRight":
+      direction = "right";
+      break;
+    default:
+      break;
+  }
+}
