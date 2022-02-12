d57ff45d8261ec28867d24333d9aee7a8a49c4b7
int2cs <gabriel.gn87@protonmail.com>
2022-02-12 11:58:43 +0100

    add 'pause' & acceleration

--- app.js
+++ app.js
@@ -4,15 +4,16 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
+let start = false;
 let direction = "right";
+let speed = 800;
 let apple = [5, 5];
 let score = 0;
 let text;
 let textWidth;
 
-const speed = 800;
 const gridElem = 40; //40 => taille de chaque case de notre grille soit 20 cases * 20 case = 800(largeur et hauteur de notre grille)
-const snake = [
+let snake = [
   [9, 9],
   [8, 9],
   [7, 9],
@@ -32,6 +33,18 @@ ctx.fillText(text, canvas.width / 2, canvas.height / 2);
 /*
   FUNCTIONS
 */
+function initGame() {
+  direction = "right";
+  apple = [5, 5];
+  speed = 800;
+  score = 0;
+
+  snake = [
+    [9, 9],
+    [8, 9],
+    [7, 9],
+  ]; // Position initiale [[x, y], [x, y], [x, y]]
+}
 function drawMap() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, 800, 800);
@@ -80,25 +93,53 @@ function isEating() {
   if (snake[0][0] === apple[0] && snake[0][1] === apple[1]) {
     generateApple();
     score++;
+    if (speed < 930) speed += 5;
     return true;
   }
 }
 
 function play() {
-  updateSnakePosition();
+  if (start) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(speed);
 
-  if (!isGameOver()) {
-    drawMap();
-    drawSnake();
-    drawApple();
-    drawScore();
+    updateSnakePosition();
 
-    setTimeout(() => {
-      requestAnimationFrame(play);
-    }, 1000 - speed);
-  } else {
-    console.log("GAME OVER !");
+    if (!isGameOver()) {
+      drawMap();
+      drawSnake();
+      drawApple();
+      drawScore();
+
+      setTimeout(() => {
+        requestAnimationFrame(play);
+      }, 1000 - speed);
+    } else {
+      gameOver();
+    }
   }
+}
+function gameOver() {
+  console.log("Game Over");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawMap();
+  text = `Game Over !`;
+  ctx.fillStyle = "white";
+  ctx.font = "30px Verdana";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "top";
+  textWidth = ctx.measureText(text);
+
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+
+  text = `Appuez sur ESPACE pour relancer une partie`;
+  ctx.fillStyle = "white";
+  ctx.font = "30px Verdana";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "top";
+  textWidth = ctx.measureText(text);
+
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2 + 100);
 }
 
 function updateSnakePosition() {
@@ -125,9 +166,21 @@ function updateSnakePosition() {
 }
 
 function keyboardEvent(e) {
+  e.preventDefault();
   switch (e.code) {
     case "Space":
-      requestAnimationFrame(play);
+      if (isGameOver()) {
+        console.log("on relance une partie");
+        initGame();
+        play();
+        return;
+      } else {
+        console.log("new game");
+        if (!start) {
+          start = true;
+          requestAnimationFrame(play);
+        } else start = false;
+      }
       break;
     case "ArrowUp":
       direction = "top";
@@ -140,6 +193,10 @@ function keyboardEvent(e) {
       break;
     case "ArrowRight":
       direction = "right";
+      break;
+    case "KeyS":
+      console.log("spedd++");
+      speed += 50;
       break;
     default:
       break;
